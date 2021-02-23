7a8ae0e7dd219d311ed68667d1e53863c296fc6e
danist7 <daniel.santo-tomas@estudiante.uam.es>
2021-02-23 18:02:22 +0100

    nuevo main

--- Practica 1/src/main.py
+++ Practica 1/src/main.py
@@ -8,25 +8,22 @@
  se ciñe a las actividades docentes de dicha asignatura.
 """
 import os
+import shutil
 import time
 from bmi.search.search import VSMDotProductSearcher, VSMCosineSearcher
 from bmi.search.whooshy import WhooshBuilder, WhooshIndex, WhooshSearcher
 
 def main():
-    index_root_dir = "./index/index_"
-    test_collection ("./data/toy/", index_root_dir + "toy", "cc", "aa dd")
+    index_root_dir = "./index/"
     collections_root_dir = "./data/collections/"
+    test_collection (collections_root_dir + "toy", index_root_dir + "toy", "cc", "aa dd")
     test_collection (collections_root_dir + "urls.txt", index_root_dir + "urls", "wikipedia", "information probability")
     test_collection (collections_root_dir + "docs1k.zip", index_root_dir + "docs", "seat", "obama family tree")
 
 def clear (index_path: str):
-    if not os.path.isdir(index_path):
-      print("Creating " + index_path)
-      os.mkdir(index_path)
-    for f in os.listdir(index_path):
-      p = os.path.join(index_path, f)
-      if os.path.isfile(p):
-        os.remove(p)
+    if os.path.exists(index_path): shutil.rmtree(index_path)
+    else: print("Creating " + index_path)
+    os.makedirs(index_path)
 
 def test_collection(collection_path: str, index_path: str, word: str, query: str):
     start_time = time.time()
@@ -39,6 +36,7 @@ def test_collection(collection_path: str, index_path: str, word: str, query: str
 
     # We now test building an index
     test_build(WhooshBuilder(index_path), collection_path)
+
     # We now inspect the index
     index = WhooshIndex(index_path)
     test_read(index, word)
@@ -47,7 +45,7 @@ def test_collection(collection_path: str, index_path: str, word: str, query: str
     print("Checking search results")
     test_search(WhooshSearcher(index_path), query, 5)
     test_search(VSMDotProductSearcher(WhooshIndex(index_path)), query, 5)
-    """test_search(VSMCosineSearcher(WhooshIndex(index_path)), query, 5)"""
+    test_search(VSMCosineSearcher(WhooshIndex(index_path)), query, 5)
 
 def test_build(builder, collection):
     stamp = time.time()
@@ -65,20 +63,20 @@ def test_build(builder, collection):
 def test_read(index, word):
     stamp = time.time()
     print("Reading index with", type(index))
+    print("Collection size:", index.ndocs())
     print("Vocabulary size:", len(index.all_terms()))
     terms = index.all_terms_with_freq()
     terms.sort(key=lambda tup: tup[1], reverse=True)
     print("  Top 5 most frequent terms:")
     for term in terms[0:5]:
-        print("\t" + str(term[0]) + "\t" + str(term[1]) + "=" + str(index.total_freq(term)))
+        print("\t" + term[0] + "\t" + str(term[1]) + "=" + str(index.total_freq(term)))
     print()
     # more tests
     doc_id = 0
     # this method provides a vector of tuples
     # for each doc: (term, freq), encapsulated in a TermFreq object (in module index)
     vector = index.doc_vector(doc_id)
-    #initialTerm = vector.size() / 2
-    initialTerm = len(vector)/ 2
+    initialTerm = len(vector) / 2
     nTerms = 5
     print("  A few term frequencies for doc_id = " + str(doc_id) + " - " + index.doc_path(doc_id) + ": ")
     i = 0
